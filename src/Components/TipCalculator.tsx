440de116c9665d755ae7a3bc792ee58be1f88c6d
Daniel <aklogdaniel@gmail.com>
2023-06-01 15:18:20 +0300

    styling with two columns

--- src/Components/TipCalculator.tsx
+++ src/Components/TipCalculator.tsx
@@ -1,9 +1,9 @@
-import { Container, NumberInput, Text } from "@mantine/core";
+import { Container, NumberInput, SimpleGrid, Text } from "@mantine/core";
 import { User } from "tabler-icons-react";
 export default function TipCalculator() {
-	const tipPercentages = ["5%", "10%", "15%", "25%", "50%", "custom"];
-	const activatePercentage = (percentage: React.MouseEvent<HTMLDivElement>) => {
-		console.log(percentage.currentTarget.innerText);
+	const tipPercentages = [5, 10, 15, 25, 50];
+	const activatePercentage = (event: React.MouseEvent<HTMLDivElement>) => {
+		console.log(event.currentTarget.dataset.percentage);
 	};
 	return (
 		<div>
@@ -11,37 +11,46 @@ export default function TipCalculator() {
 			<Text>TTER</Text>
 
 			<Container mt={40}>
-				<NumberInput
-					hideControls
-					icon="$"
-					styles={{
-						input: {
-							textAlign: "right",
-						},
-					}}
-				/>
-				<Text size="sm">Select Tip &#37;</Text>
-				<div className="select-tip">
-					{tipPercentages.map((percentage) => (
-						<div
-							className="percentage"
-							key={percentage}
-							onClick={(percentage) => activatePercentage(percentage)}>
-							{percentage}
+				<SimpleGrid
+					cols={2}
+					breakpoints={[{ maxWidth: "45rem", cols: 1, spacing: "sm" }]}>
+					<div>
+						<NumberInput
+							hideControls
+							icon="$"
+							styles={{
+								input: {
+									textAlign: "right",
+								},
+							}}
+						/>
+						<Text size="sm">Select Tip &#37;</Text>
+						<div className="select-tip">
+							{tipPercentages.map((percentage) => (
+								<div
+									className="percentage"
+									key={percentage}
+									data-percentage={percentage}
+									onClick={(event) => activatePercentage(event)}>
+									{percentage}&#37;
+								</div>
+							))}
+							<div className="custom">Custom</div>
 						</div>
-					))}
-				</div>
 
-				<Text>Number of People</Text>
-				<NumberInput
-					hideControls
-					icon={<User />}
-					styles={{
-						input: {
-							textAlign: "right",
-						},
-					}}
-				/>
+						<Text>Number of People</Text>
+						<NumberInput
+							hideControls
+							icon={<User />}
+							styles={{
+								input: {
+									textAlign: "right",
+								},
+							}}
+						/>
+					</div>
+					<div></div>
+				</SimpleGrid>
 			</Container>
 		</div>
 	);
